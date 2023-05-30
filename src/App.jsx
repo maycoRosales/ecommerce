615b6a0494f2d1be7def575f3888962c21988aec
maycoRosales <maycor@gmail.com>
2023-05-30 16:10:17 -0300

    clean app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,10 @@ import NavBar from './components/NavBar';
 import ItemListConteiner from './components/ItemListConteiner';
 
 const App = () => {
-  // const [count, setcount] = useState(0);
-  
-  // const handleRestar = () =>{
-  //   setcount(count - 1);
-  // }
-  // const handleSumar = () =>{
-  //   setcount(count + 1);
-  // }
   return (
     <div className="App">
       <NavBar />
       <ItemListConteiner greeting="Welcome to BurgerShop🍔"/>
-      {/* <div style={{display: "flex", justifyContent: "center", alignItems:"center", width: "100vw", height: "100vh",background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)"}}>
-        <button onClick={handleRestar}>-</button>
-        <span>{count}</span>
-        <button onClick={handleSumar}>+</button>
-      </div> */}
     </div>
   );
 }
